7cb286d9e6b5164813e983a1e0818e5c809f8e8c
Achuthan R <r.achuthan2001@gmail.com>
2021-03-25 21:02:47 +0530

    Create intents for different activities. Add Police and User activities.

--- app/src/main/java/com/vision/SignInActivity.java
+++ app/src/main/java/com/vision/SignInActivity.java
@@ -2,13 +2,43 @@ package com.vision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private TextView text;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        text = (TextView) findViewById(R.id.signup_text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+        button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserActivity();
+            }
+        });
+    }
+
+    public void openUserActivity() {
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+    }
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
