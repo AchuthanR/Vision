f669a3347adc5a4c107501f70f76c73a9c03c27a
aadityarock2000 <aadityaparthasarathy@gmail.com>
2021-03-26 14:20:16 +0530

    implement logout in user page

--- app/src/main/java/com/vision/UserActivity.java
+++ app/src/main/java/com/vision/UserActivity.java
@@ -1,14 +1,63 @@
 package com.vision;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class UserActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.logout3).setOnClickListener(this);
+    }
+
+    private void Logout() {
+        Log.i("Srini","insidelogout");
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(UserActivity.this, SignInActivity.class));
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.logout3:
+            {
+                Log.i("Srini","inside the onclick listener");
+                Logout();
+            }
+        }
+
     }
 }
